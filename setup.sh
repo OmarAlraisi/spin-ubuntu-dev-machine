3b327259104156bd728d079672ff3e487400a827
omar <o.r.raisi@gmail.com>
2023-08-15 07:11:45 +0000

    hmm again

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    apt install ansible
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install ansible
+if [[ "$OSTYPE" == "linux-gnu"* && -f /etc/lsb-release ]]; then
+    source /etc/lsb-release
+    if [[ "$DISTRIB_ID" == "Ubuntu" ]]; then
+        apt update
+        apt install -y ansible
+        ansible-pull -U https://github.com/OmarAlraisi/spin-ubuntu-dev-machine.git -K
+    else
+        echo "This script is intended for Ubuntu Linux only."
+        exit 1
+    fi
+else
+    echo "This script is intended for Ubuntu Linux only."
+    exit 1
 fi
-
-ansible-pull -U https://github.com/OmarAlraisi/spin-ubuntu-dev-machine.git -K
